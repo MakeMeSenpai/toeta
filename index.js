25488456df984376e88080332c3fa37b4116db5d
Anthony <sean.a.protho1@gmail.com>
2021-01-11 12:38:34 -0800

    attempt 2

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// secret values config
-let key = "undefined"
-if (config) {
-    key = config.API_KEY
-} else {
-    const heroku = require('aws-sdk')
-    key = process.env.API_KEY
-}
-
 // input
 const searched = document.getElementById("search").value
 
@@ -15,7 +6,7 @@ const srcLink = document.getElementById("source-link")
 const divOut = document.getElementById("output")
 
 // ran in getRec
-function getSource(id) {
+function getSource(id, key) {
     $.ajax({
         url: `https://api.spoonacular.com/recipes/${id}/information?apiKey=${key}`,
         success: (res) => {
@@ -27,12 +18,20 @@ function getSource(id) {
 
 // onclick
 function getRec() {
+    // secret values config
+    let key = "undefined"
+    if (config) {
+        key = config.API_KEY
+    } else {
+        const heroku = require('aws-sdk')
+        key = process.env.API_KEY
+    }
     $.ajax({
         // for now we only want 1 result, later 7 for a week, or 30 for a month -> number=1
         url: `https://api.spoonacular.com/recipes/search?apiKey=${key}&number=1&query=${searched}`,
         success: (res) => {
             divOut.innerHTML = `<h2>${res.results[0].title}</h2> <br> <img src='${res.baseUri}${res.results[0].image}'> <br><p> Ready in ${res.results[0].readyInMinutes} min.`;
-            getRec(res.results[0].id);
+            getRec(res.results[0].id, key);
         }
     });
 }
