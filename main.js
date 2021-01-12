6cd46dd43c443ebc015ae1cac7b9d7b80d3e6404
Anthony <sean.a.protho1@gmail.com>
2021-01-12 10:08:21 -0800

    working on routes

--- main.js
+++ main.js
@@ -2,10 +2,7 @@
 function isValid() {
     const password = document.getElementById("password");
     const validator = document.getElementById("validator");
-    console.log("!!!!!!!!!!!")
-    console.log(password.value)
-    console.log(validator.value)
-    if  (password.value === validator.value)  {
+    if (password.value === validator.value) {
         let path = "https://toeta.herokuapp.com/pan.html"
         if (config.local == true) {
             path = "http://127.0.0.1:5500/pan.html"
