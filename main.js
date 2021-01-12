7899a3fc635c1450dff23245f88f756fbe695a5b
Anthony <sean.a.protho1@gmail.com>
2021-01-12 10:28:55 -0800

    attempt 3

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ function isValid() {
     const password = document.getElementById("password");
     const validator = document.getElementById("validator");
     if (password.value === validator.value) {
-        const path = "https://toeta.herokuapp.com/pan.html"
-        // local path = "http://127.0.0.1:5500/pan.html"
+        let path = "pan.html"
+        if (typeof config.local !== "undefined") {
+            path = "http://127.0.0.1:5500/pan.html"
+        }
         // should relocate our user onto pandora page
         window.location.replace(`${path}`)
     } else {
