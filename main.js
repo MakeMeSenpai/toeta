3a748e659e632ed47a75b66d0ab89bcead56c619
Anthony <sean.a.protho1@gmail.com>
2021-01-12 10:53:19 -0800

    attempt 5

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 // onclick
 function isValid() {
+    const email = document.getElementById("email");
     const password = document.getElementById("password");
     const validator = document.getElementById("validator");
+    // if (email.value.includes(".")) {
     if (password.value === validator.value) {
-        let path = "pan.html"
-        if (typeof config.local !== "undefined") {
-            path = "http://127.0.0.1:5500/pan.html"
-        }
-        // should relocate our user onto pandora page
-        window.location.replace(`${path}`)
+        // if (typeof config.local !== "undefined") {
+        //     console.log("local")
+        //     // should relocate our user onto pandora page
+        //     window.location = "http://127.0.0.1:5500/pan.html"
+        // } else {
+        console.log("live")
+        // replace works for local, while this works for server side
+        window.location = "https://toeta.herokuapp.com/pan.html"
+        // }
     } else {
         alert("Your Passwords do not match!")
     }
+    // } else {
+    //     alert("Please enter a valid Email")
+    // }
 }
